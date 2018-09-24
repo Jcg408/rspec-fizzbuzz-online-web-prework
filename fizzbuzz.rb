06e6a08a96afd9efc1fc6c815a2f466df589c2ac
Jessica Gladstone <jcg321@comcast.net>
2018-09-24 20:41:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,10 +6,11 @@
 def fizzbuzz(num)
   if num % 3 == 0
     return "Fizz"
+    
   elsif num % 5 == 0
     return "Buzz"
     
-  elsif num % 3 == 0 && num % 5 == 0
+  elsif num % 3 && num % 5 == 0
     return "FizzBuzz"
    
   end
